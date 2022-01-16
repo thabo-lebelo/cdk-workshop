b8e95e4c506fc62348a6b20df358dc47951ae782
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-16 15:20:54 +0200

    Update CDK Deploy Entrypoint

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+/*
 import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
 const app = new cdk.App();
 new CdkWorkshopStack(app, 'CdkWorkshopStack');
+*/
+
+// Since the purpose of our pipeline is to deploy our application stack, we no longer want the main CDK application to deploy our original app. 
+
+import { WorkshopPipelineStack } from '../lib/pipeline-stack';
+
+const app = new cdk.App();
+new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack');
